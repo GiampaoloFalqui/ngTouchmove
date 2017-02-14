12779c8d64e62ab6fdb7a0d113add6e5dfdade46
Giampaolo Falqui <giampaolofalqui@gmail.com>
2017-02-14 21:19:54 +0100

    Removes event.preventDefault()

--- src/ngTouchmove.js
+++ src/ngTouchmove.js
@@ -4,21 +4,21 @@ angular.module("ngTouchmove", []).directive("ngTouchmove", function () {
   return {
     controller: function ($scope, $element, $attrs) {
       $element.bind('touchstart', onTouchStart);
-      
+
       function onTouchStart(event) {
-        event.preventDefault();
+        // event.preventDefault();
         $element.bind('touchmove', onTouchMove);
         $element.bind('touchend', onTouchEnd);
       };
-      
+
       function onTouchMove(event) {
           var method = $element.attr('ng-touchmove');
           $scope.$event = event;
           $scope.$apply(method);
       };
-      
+
       function onTouchEnd(event) {
-        event.preventDefault();
+        // event.preventDefault();
         $element.unbind('touchmove', onTouchMove);
         $element.unbind('touchend', onTouchEnd);
       };
